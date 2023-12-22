10600c259741ec38a460ad682ba192df92ad6cdf
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-12-21 17:16:41 -0700

    basic set up for authorization server is added

---
+++ authorization/src/test/java/com/learn/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
