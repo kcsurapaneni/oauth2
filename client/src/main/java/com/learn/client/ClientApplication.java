91bc97c88b383b2c7aaf688eadc25fd36372432d
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-12-23 16:59:16 -0700

    client application added

---
+++ client/src/main/java/com/learn/client/ClientApplication.java
@@ -0,0 +1,14 @@
+package com.learn.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
