4078f701dc1d557f0e7c3792bca41823bf3e1482
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-12-23 18:59:25 -0700

    routing configuration is added

---
+++ client/src/main/java/com/learn/client/config/ApplicationConfiguration.java
@@ -0,0 +1,23 @@
+package com.learn.client.config;
+
+import org.springframework.cloud.gateway.route.*;
+import org.springframework.cloud.gateway.route.builder.*;
+import org.springframework.context.annotation.*;
+
+/**
+ * @author Krishna Chaitanya
+ */
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    RouteLocator gateway(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder
+                .routes()
+                .route(route -> route.path("/hello")
+                        .filters(GatewayFilterSpec::tokenRelay)
+                        .uri("http://localhost:8081/hello"))
+                .build();
+    }
+
+}
