bddc49e3e415b7b20683c8265616526f0303fafb
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2024-02-24 19:00:10 -0700

    reference link is added

--- generate_keys.sh
+++ generate_keys.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# https://developers.yubico.com/PIV/Guides/Generating_keys_using_OpenSSL.html
 # Generate an RSA private key, of size 2048, and output it to a file named key.pem
 openssl genrsa -out key.pem 2048
 # Extract the public key from the key pair, which can be used in a certificate
