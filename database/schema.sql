a410c6b79fdf619b8655042560adacb1f12206da
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2024-02-26 13:35:58 -0700

    user management create (DDL) scripts(user, role and user_role) are added

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,45 @@
+-- DROP
+
+drop table if exists user_roles cascade;
+drop table if exists user cascade;
+drop table if exists role cascade;
+
+drop table if exists oauth2_registered_client cascade;
+
+-- CREATE
+
+-- user
+
+create table user (
+    is_active boolean default true not null,
+    created_at timestamp default current_timestamp not null,
+    id bigint not null,
+    address varchar(255) not null,
+    name varchar(255) not null,
+    password varchar(255) not null,
+    username varchar(255) not null unique,
+    primary key (id)
+);
+
+
+-- role
+
+create table role (
+    is_active boolean default true not null,
+    id bigint not null,
+    name varchar(255) unique,
+    primary key (id)
+);
+
+
+-- user_roles
+
+create table user_role (
+    user_id bigint not null,
+    role_id bigint not null,
+    primary key (user_id, role_id)
+);
+
 
 -- oauth2_registered_client
 
