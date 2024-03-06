c0a5af244949a9d138846c5f53aaf39d0aed04f3
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2024-03-06 11:07:48 -0700

    oauth2-registered-client DDL files(create and drop) are added

---
+++ client/src/main/resources/sql/oauth2-registered-client-schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE oauth2_registered_client (
+    id varchar(100) NOT NULL,
+    client_id varchar(100) NOT NULL,
+    client_secret varchar(200) DEFAULT NULL,
+    client_authentication_method varchar(1000) NOT NULL,
+    authorization_grant_type varchar(1000) NOT NULL,
+    authorization_uri varchar(1000) NOT NULL,
+    token_uri varchar(1000) DEFAULT NULL,
+    redirect_uri varchar(1000) DEFAULT NULL,
+    scopes varchar(1000) NOT NULL,
+    issuer_uri varchar(1000) DEFAULT NULL,
+    jwk_set_uri varchar(1000) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
