db1816fcd545305afed7317e81790125d49d1c6c
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2024-03-08 10:07:05 -0700

    using jdbc instead of reactive jdbc

--- client/src/main/java/com/learn/client/repository/JdbcClientRegistrationRepository.java
+++ client/src/main/java/com/learn/client/repository/JdbcClientRegistrationRepository.java
@@ -14,7 +14,7 @@ import java.util.stream.*;
  * @author Krishna Chaitanya
  */
 @Repository
-public class JdbcReactiveClientRegistrationRepository implements ReactiveClientRegistrationRepository, Iterable<ClientRegistration> {
+public class JdbcClientRegistrationRepository implements ClientRegistrationRepository, Iterable<ClientRegistration> {
 
     private static final String FETCH = """
             SELECT id,
@@ -34,18 +34,18 @@ public class JdbcReactiveClientRegistrationRepository implements ReactiveClientR
     private static final String FETCH_BY_ID = FETCH + " WHERE id = ?";
     private final JdbcTemplate jdbcTemplate;
 
-    public JdbcReactiveClientRegistrationRepository(JdbcTemplate jdbcTemplate) {
+    public JdbcClientRegistrationRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
-    public Mono<ClientRegistration> findByRegistrationId(String registrationId) {
-        return Mono.justOrEmpty(jdbcTemplate.query(FETCH_BY_ID, this::mapToClient, registrationId).get(0));
+    public ClientRegistration findByRegistrationId(String registrationId) {
+        return jdbcTemplate.query(FETCH_BY_ID, this::mapToClient, registrationId).get(0);
     }
 
     private ClientRegistration mapToClient(ResultSet rs, int rowNum) throws SQLException {
-        return ClientRegistration
-                .withRegistrationId(rs.getString("id"))
+        return ClientRegistrations.fromIssuerLocation(rs.getString("issuer_uri"))
+                .registrationId(rs.getString("id"))
                 .clientId(rs.getString("client_id"))
                 .clientName(rs.getString("client_name"))
                 .clientSecret(rs.getString("client_secret"))
@@ -59,8 +59,8 @@ public class JdbcReactiveClientRegistrationRepository implements ReactiveClientR
                 .tokenUri(rs.getString("token_uri"))
                 .redirectUri(rs.getString("redirect_uri"))
                 .scope(Arrays.stream(rs.getString("scopes").split(",")).map(String::trim).collect(Collectors.toSet()))
-                .issuerUri(rs.getString("issuer_uri"))
-                .jwkSetUri(rs.getString("jwk_set_uri"))
+//                .issuerUri(rs.getString("issuer_uri"))
+//                .jwkSetUri(rs.getString("jwk_set_uri"))
                 .build();
     }
 
