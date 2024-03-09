f1c888d37b0d6cd8539c2c69b5457061b6c925b8
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2024-03-09 10:37:37 -0700

    as gateway dependecies are removed, commented out the code

--- client/src/main/java/com/learn/client/config/ApplicationConfiguration.java
+++ client/src/main/java/com/learn/client/config/ApplicationConfiguration.java
@@ -1,23 +1,19 @@
 package com.learn.client.config;
 
-import org.springframework.cloud.gateway.route.*;
-import org.springframework.cloud.gateway.route.builder.*;
-import org.springframework.context.annotation.*;
-
 /**
  * @author Krishna Chaitanya
  */
-@Configuration
+//@Configuration
 public class ApplicationConfiguration {
 
-    @Bean
-    RouteLocator gateway(RouteLocatorBuilder routeLocatorBuilder) {
-        return routeLocatorBuilder
-                .routes()
-                .route(route -> route.path("/hello")
-                        .filters(GatewayFilterSpec::tokenRelay)
-                        .uri("http://localhost:7070/hello"))
-                .build();
-    }
+//    @Bean
+//    RouteLocator gateway(RouteLocatorBuilder routeLocatorBuilder) {
+//        return routeLocatorBuilder
+//                .routes()
+//                .route(route -> route.path("/hello")
+//                        .filters(GatewayFilterSpec::tokenRelay)
+//                        .uri("http://localhost:7070/hello"))
+//                .build();
+//    }
 
 }
